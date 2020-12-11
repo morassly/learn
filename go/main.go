a5f5fee6aa33d9ea0ad50da98dc238d3b5415722
lymorass <lymorass@gmail.com>
2020-12-11 14:11:39 +0800

    add .gitignore and sth new

--- go/main.go
+++ go/main.go
@@ -2,23 +2,10 @@ package main
 
 import(
     "fmt"
-    "math/rand"
-    "time"
+    "strings"
 )
 
 func main(){
-    for i:=0; i<10 ;i++{
-        a:=rand.Int()
-        fmt.Printf("%d / ",a)
-    }
-    for i := 0; i < 5; i++ {
-        r:= rand.Intn(8)
-        fmt.Printf("%d /",r)
-    }
-    fmt.Println()
-	timens := int64(time.Now().Nanosecond())
-	rand.Seed(timens)
-	for i := 0; i < 10; i++ {
-		fmt.Printf("%2.2f / ", 100*rand.Float32())
-	}
+    str := "This is an example of a string"
+    fmt.Println(strings.HasPrefix(str,"Th"))
 }
