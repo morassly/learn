42a6c2fc1a44045cec70c54e70eccd9b11584911
lymorass <lymorass@gmail.com>
2021-04-14 09:42:52 +0800

    .

---
+++ leetcode/0530.go
@@ -0,0 +1,32 @@
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func minDiffInBST1(root *TreeNode) int {
+	ans, pre := 10000000, -1
+	var dfs func(*TreeNode)
+	dfs = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		dfs(root.Left)
+		if pre == -1 {
+			pre = root.Val
+		} else {
+			if ans > (root.Val - pre) {
+				ans = root.Val - pre
+			}
+			pre = root.Val
+		}
+		dfs(root.Right)
+	}
+	dfs(root)
+	return ans
+}
