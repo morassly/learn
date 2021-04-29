ff466e063e8ead7b729653c5fcc33a655834cb0b
lymorass <lymorass@gmail.com>
2021-04-29 17:46:39 +0800

    fmt cpp

--- leetcode/1011.cpp
+++ leetcode/1011.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int D) {
-        int left = *max_element(weights.begin(), weights.end()),
-            right = accumulate(weights.begin(), weights.end(), 0);
+        int left = *max_element(weights.begin(), weights.end()), right = accumulate(weights.begin(), weights.end(), 0);
         while (left < right) {
             int mid = (left + right) / 2;
             int need = 1, cur = 0;
