2c2a8d668d287a3a3c0a3156f7959d481e8794ec
lymorass <lymorass@gmail.com>
2021-05-27 20:58:29 +0800

    leetcode .

--- leetcode/1787.go
+++ leetcode/1787.go
@@ -6,7 +6,34 @@ const (
 )
 
 func minChanges(nums []int, k int) int {
+	n, dp := len(nums), make([]int, N)
+	for i := range dp {
+		dp[i] = INF
+	}
+	dp[0] = 0
+	ans, minv := 0, INF
+	for i := 0; i < k; i++ {
+		cnt, len, maxv := make(map[int]int), 0, 0
+		for j := i; j < n; j += k {
+			cnt[nums[j]]++
+			len++
+			maxv = max(maxv, cnt[nums[j]])
+		}
+		ans += len - maxv
+		minv = min(minv, maxv)
 
+		dp1 := make([]int, N)
+		for i := range dp {
+			dp1[i] = INF
+		}
+		for j := 0; j < N; j++ {
+			for x, cntx := range cnt {
+				dp1[j] = min(dp1[j], dp[j^x]+len-cntx)
+			}
+		}
+		dp = dp1
+	}
+	return min(dp[0], ans+minv)
 }
 func min(i, j int) int {
 	if i > j {
